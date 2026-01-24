135394e81baa8a441e0523877a3126719df85f14
SeaHuyty <seahuyty4444@gmail.com>
2026-01-24 10:23:30 +0700

    Create: repository for property model

--- lib/features/property/repositories/property_repository.dart
+++ lib/features/property/repositories/property_repository.dart
@@ -11,6 +11,47 @@ class PropertyRepository {
     final response = await _supabase.from('properties').select();
     return (response as List).map((e) => PropertyModel.fromJson(e)).toList();
   }
+
+  Future<PropertyModel> getPropertyById(String id) async {
+    final response = await _supabase
+        .from('properties')
+        .select()
+        .eq('id', id)
+        .single();
+    return PropertyModel.fromJson(response);
+  }
+
+  Future<List<PropertyModel>> getPropertiesByLandlordId(
+    String landlordId,
+  ) async {
+    final response = await _supabase
+        .from('properties')
+        .select()
+        .eq('landlord_id', landlordId);
+    return (response as List).map((e) => PropertyModel.fromJson(e)).toList();
+  }
+
+  Future<bool> createProperty(PropertyModel property) async {
+    try {
+      await _supabase
+          .from('properties')
+          .insert(property.toJson());
+
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  Future<bool> updateProperty(PropertyModel property) async {
+    try {
+      await _supabase.from('properties').update(property.toJson()).eq('id', property.id);
+
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
 }
 
 final propertyRepositoryProvider = Provider<PropertyRepository>((ref) {

63d3a634f6f480e07a09eb5bfa3392ff0632d841
Phay Someth <phay.someth70@gmail.com>
2026-03-13 10:39:01 +0700

    chore: add cached_network_image, url_launcher deps and mapbox satellite config

--- lib/core/utils/app_config.dart
+++ lib/core/utils/app_config.dart
@@ -6,4 +6,7 @@ class AppConfig {
 
   static String get mapboxTileUrl =>
       'https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/256/{z}/{x}/{y}?access_token=$mapboxToken';
+
+  static String get mapboxSatelliteUrl =>
+      'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/tiles/256/{z}/{x}/{y}?access_token=$mapboxToken';
 }
